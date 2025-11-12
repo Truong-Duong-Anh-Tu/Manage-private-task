f4de13ca2f3df8141441f53a919bc7a2b1052028
Truong-Duong-Anh-Tu <truongtu11747@gmail.com>
2025-11-12 15:42:19 +0700

    organize folders

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Home from './pages/Home/home.jsx'
+
+function App() {
+
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/pages/Home/home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './style.css'
+
+function Home() {
+
+    return (
+        <div className='homepage'>
+            <div className='taskbar'>
+
+            </div>
+
+            <div className='content'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
