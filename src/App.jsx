87d9ad7a89c084a85833934a4891bd1ffddfcb95
Truong-Duong-Anh-Tu <truongtu11747@gmail.com>
2025-11-12 11:44:23 +0700

    Init project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <p>Place holder for the app</p>
+    </div>
+  )
+}
+
+export default App
